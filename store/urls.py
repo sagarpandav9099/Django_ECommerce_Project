7cc9cd36fd50cb3f16360a1f41422232f5918a3b
sagarpandav9099 <141428515+sagarpandav9099@users.noreply.github.com>
2023-12-14 13:37:04 +0530

    Delete store directory

--- store/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.store, name='store'),
-    path('product/<slug:product_slug>', views.product_info, name='product-info'),
-    path('search/<slug:category_slug>', views.list_category, name='list-category'),
-]
